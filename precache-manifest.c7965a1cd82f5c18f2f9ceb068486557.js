cdfc021a2f78c6480c6eb308dc77b25e641598c1
neitiris <Veliantos@gmail.com>
2019-07-13 21:31:44 +0300

    Updates

---
+++ precache-manifest.c7965a1cd82f5c18f2f9ceb068486557.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7889ee43247c56f1171817e5ecb4ff61",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_list-of-goods/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3ddfaa436b9e722a9131",
+    "url": "/react_list-of-goods/static/js/main.849226a6.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.c7965a1cd82f5c18f2f9ceb068486557.js"
 );
 
 self.addEventListener('message', (event) => {
